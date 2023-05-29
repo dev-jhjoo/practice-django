067c63a2d790463a899410436b2960311465b479
jh.joo <dev.jh.joo@gmail.com>
2023-05-29 21:45:19 +0900

    fix requirements.txt fix settings.py

--- practice_django/settings.py
+++ practice_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--3c@hcgjbli7t058ro4x=kx)tqquc&lwmc2b9qfr^#ee+uqh2=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
