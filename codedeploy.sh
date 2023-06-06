33d1898fac3f722b6b638310acd403185dd9d5cf
JEEHOON JOO <dev.jh.joo@gmail.com>
2023-06-06 23:22:49 +0900

    add deploy shell

---
+++ codedeploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CURRENT_PID=$(lsof -i :8000 -t)
+echo "> CURRENT_PID: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  ehco "> Not currently running"
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -9 "$CURRENT_PID"
+  sleep 2
+fi
+
+echo "> pip3 install -r ./practice-django/requirements.txt"
+pip3 install -r ./practice-django/requirements.txt
+sleep 2
+
+echo "> run server"
+nohup python3 ./practice-django/manage.py runserver 0.0.0.0:8000 >/dev/null 2>&1 &
